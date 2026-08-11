fe98cb734548eff3720761df7d8bfbc0200dbefb
Cid Vieira <cidvieira99@gmail.com>
2026-08-11 13:46:30 -0300

    build: setup static export for cervejaria-jaobrelao and separate source code

---
+++ 2024/cervejaria-jaobrelao-code/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio-archive/2024/cervejaria-jaobrelao',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
