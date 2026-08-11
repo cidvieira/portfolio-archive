944b661a29ab9f663e735f44b2122048298b4cf3
Cid Vieira <cidvieira99@gmail.com>
2026-08-11 19:47:23 -0300

    fix: static export new path /portfolio-archive/2024/cervejaria-jaobrelao

--- 2024/cervejaria-jaobrelao-code/next.config.mjs
+++ 2024/cervejaria-jaobrelao-code/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/2024/cervejaria-jaobrelao',
+  basePath: '/portfolio-archive/2024/cervejaria-jaobrelao',
   images: {
     unoptimized: true,
   },
